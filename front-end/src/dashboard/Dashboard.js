d7776ca73852a4b7d1056a687f9ab8adddbe2f70
Christopher Roddy <chrxtopherxddy@gmail.com>
2022-02-06 19:55:03 -0700

    Dashboard displays a list of all reservations

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -23,17 +23,36 @@ function Dashboard({ date }) {
     return () => abortController.abort();
   }
 
+  const reservationsList = reservations.map((reservation) => {
+    return (
+      <div className="card bg-light border-dark m-1">
+        <div className="card-body">
+          <h4 className="card-title text-center">
+            {reservation.first_name} {reservation.last_name} : Party of{" "}
+            {reservation.people}
+          </h4>
+          <p className="card-text text-center">
+            <strong>Contact:</strong> {reservation.mobile_number}
+          </p>
+          <p className="card-text text-center">
+            <strong>Date:</strong> {reservation.reservation_date}
+          </p>
+          <p className="card-text text-center">
+            <strong>Time:</strong> {reservation.reservation_time}
+          </p>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <main>
-      <h1 className="display-4">Dashboard</h1>
+      <h1 className="display-4 text-center">Dashboard</h1>
       <div className="d-md-flex mb-3">
         <h4 className="mb-0">Reservations for date: {date}</h4>
       </div>
       <ErrorAlert error={reservationsError} />
-      <div>
-        <p className="mb-1"> below should be a list of all reservations</p>
-        {JSON.stringify(reservations)}
-      </div>
+      <div>{reservationsList}</div>
     </main>
   );
 }
