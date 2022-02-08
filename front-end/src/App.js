663fd3849d8882980f87ebe5afb233584af1d18e
Christopher Roddy <chrxtopherxddy@gmail.com>
2022-02-07 22:29:53 -0700

    Form for '/reservations/new' has been created
    Cancel button returns user to the home page, submit button does not have functionality yet

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Layout from "./layout/Layout";
+import ReservationForm from "./reservations/CreateReservation";
 
 /**
  * Defines the root application component.
@@ -9,8 +10,11 @@ import Layout from "./layout/Layout";
 function App() {
   return (
     <Switch>
-      <Route path="/">
+      <Route exact path="/dashboard">
         <Layout />
+      </Route>
+      <Route exact path="/reservations/new">
+        <ReservationForm />
       </Route>
     </Switch>
   );
