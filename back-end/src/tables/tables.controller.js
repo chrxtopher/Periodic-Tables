97528c5c42bddceb7df5b8c17727b51a0589d715
Christopher Roddy <chrxtopherxddy@gmail.com>
2022-02-15 14:56:55 -0700

    GET request to '/tables' will list all tables in the database
    sorted by table name

--- back-end/src/tables/tables.controller.js
+++ back-end/src/tables/tables.controller.js
@@ -1,3 +1,10 @@
 const tablesService = require("./tables.service");
 
-module.exports = {};
+async function list(req, res) {
+  const data = await tablesService.list();
+  res.json({ data });
+}
+
+module.exports = {
+  list,
+};
