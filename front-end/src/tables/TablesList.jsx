3a3040e76eb0dceddec24d72c2075b8453c87d2d
Christopher Roddy <chrxtopherxddy@gmail.com>
2022-02-20 12:01:21 -0700

    formatting update

--- front-end/src/tables/TablesList.jsx
+++ front-end/src/tables/TablesList.jsx
@@ -1,26 +1,36 @@
 import React from "react";
 
 function TablesList({ tables, reservations }) {
-
   return tables.map((table) => {
     const reservation = reservations.find((reservation) => {
-      return reservation.reservation_id === table.reservation_id
-    })
+      return reservation.reservation_id === table.reservation_id;
+    });
 
-    const status = reservation ? <strong>Occupied by: {reservation.first_name} {reservation.last_name}</strong> : "Free"
+    const status = reservation ? (
+      <strong>
+        Occupied by: {reservation.first_name} {reservation.last_name}
+      </strong>
+    ) : (
+      "Free"
+    );
 
     return (
-        <div key={table.table_id} className="card bg-light border-dark m-4 shadow">
-          <div className="card-body">
-            <h4 className="card-title text-center">{table.table_name}</h4>
-            <p className="card-text text-center">
+      <div
+        key={table.table_id}
+        className="card bg-light border-dark m-4 shadow"
+      >
+        <div className="card-body">
+          <h4 className="card-title text-center">{table.table_name}</h4>
+          <p className="card-text text-center">
             <strong>Capacity:</strong> {table.capacity}
-            </p>
-            <p className="text-center" data-table-id-status={`${table.table_id}`}>{status}</p>
-          </div>
+          </p>
+          <p className="text-center" data-table-id-status={`${table.table_id}`}>
+            {status}
+          </p>
         </div>
-    )
-  })
+      </div>
+    );
+  });
 }
 
 export default TablesList;
