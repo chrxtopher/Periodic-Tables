21d8ca51a83924ab1e1cfd54b15241b58c7dae54
Christopher Roddy <chrxtopherxddy@gmail.com>
2022-02-26 03:16:41 -0700

    implemented methodNotAllowed.js
    Will respond with an error when http method requests are made that are not defined.

--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -1,12 +1,18 @@
 const router = require("express").Router();
 const controller = require("./tables.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/").get(controller.list).post(controller.create);
+router
+  .route("/")
+  .get(controller.list)
+  .post(controller.create)
+  .all(methodNotAllowed);
 
 router
   .route("/:table_id/seat")
   .get(controller.read)
   .put(controller.update)
-  .delete(controller.finishTable);
+  .delete(controller.finishTable)
+  .all(methodNotAllowed);
 
 module.exports = router;
