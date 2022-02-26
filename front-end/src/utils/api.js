a3a38aa0bc874072ea7e0db4bac369666b770d4b
Christopher Roddy <chrxtopherxddy@gmail.com>
2022-02-25 22:01:53 -0700

    deployment

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -5,7 +5,7 @@
 import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
-const API_BASE_URL = "http://localhost:3500";
+const API_BASE_URL = "https://periodictables-server.herokuapp.com";
 
 //  process.env.REACT_APP_API_BASE_URL || "http://localhost:3500";
 
