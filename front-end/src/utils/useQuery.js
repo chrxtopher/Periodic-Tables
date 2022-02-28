cd6fca482dce02e5a2d6ccec42428aa57e8d0149
Christopher Roddy <chrxtopherxddy@gmail.com>
2022-02-28 09:45:14 -0700

    push to main for vercel update

--- front-end/src/utils/useQuery.js
+++ front-end/src/utils/useQuery.js
@@ -1,8 +1,6 @@
 /**
  * useQuery is a custom hook that makes use of the useLocation and the URL class to parse the query parameters.
- *
  * @example
- *
  *     const query = useQuery();
  *     const date = query.get("date")
  */
